0f888ce2e871b9df96fff4843d3f9fc3e1d3864c
Husnainshk <husnainprince333@gmail.com>
2021-12-03 00:20:53 +0500

    Initial Commit

---
+++ core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def projects(request):
+    return render(request, 'core/projects.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def single(request):
+    return render(request, 'core/single.html')
---
+++ zillow_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\TOSHIBA\Desktop\zillow_project\zillow_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
